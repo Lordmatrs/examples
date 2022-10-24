a5f1d7451670a9e7a972b6e1236bec7b2d53b9cf
mikhail <1frolovmv1@gmail.com>
2022-10-24 14:15:56 +0300

    Третья задачка с Сергеем на Лекции

---
+++ examples003sum/Program.cs
@@ -0,0 +1,8 @@
+﻿// написать программу которая генерирует два целых ыисла и выводитих сумму
+
+Console.Clear();
+int numberA = 24;
+int numberB = 42;
+int result = numberA + numberB;
+Console.WriteLine(result);
+
