328f61be528a19f5dcded24af8344ae458d43770
mikhail <1frolovmv1@gmail.com>
2022-10-24 19:43:34 +0300

    Узнаем мах среди разных чисел

---
+++ examples006/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int a = 1;
+int b = 6;
+int c = 2;
+int d = 58;
+int e = 40;
+
+int max = 0;
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
+
+
