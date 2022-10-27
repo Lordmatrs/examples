124f48ed7f35a4f42ffe409a66ac42f1042bb79f
mikhail <1frolovmv1@gmail.com>
2022-10-27 12:54:54 +0300

    семинар1

---
+++ seminar1/Program.cs
@@ -0,0 +1,75 @@
+﻿// напишите программу которая принемает на вход два
+// числа и проверяет является ли первое число квадратом второго
+
+// Console.Clear();
+// Console.WriteLine("Ввидите число:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Ввидите число:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if(num1 == num2*num2)    
+// {
+//     Console.WriteLine("первое число является квадратом второго числа");
+// }
+// else
+// {
+//     Console.WriteLine("первое число не является квадратом второго числа");
+// }
+
+// Напишите программу которая будет выдавать название дня недели по заданному номеру
+// Console.Clear();
+// Console.WriteLine("Ввидите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if(num == 1)
+// {
+//    Console.WriteLine("день недели понедельник");
+// }
+// else
+// if(num == 2)
+// {
+//    Console.WriteLine("день недели вторник");
+// }
+// else
+// if(num == 3)
+// {
+//    Console.WriteLine("день недели среда");
+// }
+// else
+// if(num == 4)
+// {
+//    Console.WriteLine("день недели четверг");
+// }
+// else
+// if(num == 5)
+// {
+//    Console.WriteLine("день недели пятница");
+// }
+// else
+// if(num == 6)
+// {
+//    Console.WriteLine("день недели суббота. Выходной день");
+// }
+// else
+// if(num == 7)
+// {
+//    Console.WriteLine("день недели воскресенье. Выходной день");
+// }
+// else
+// {
+//     Console.WriteLine("нет такого дня недели");
+// }
+// Напишите программу которая принимет трехзначное число и на выходе показывает последнюю цифру данного числа
+// Console.Clear();
+// Console.WriteLine("Ввидите трехзначное число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if((num>99) & (num<1000))
+// {
+//     int num1 = num % 10;
+//     Console.WriteLine($"Последняя цифра трехзначного числа: {num1}");
+// }
+// else
+// {
+//     Console.WriteLine("это не трехзначное число");
+// }
+
+
